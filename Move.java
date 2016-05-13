0b50a7abd94f7356de1e7d8273fb7507f1d58a9f
yohance-lorrington <tabahanih@gmail.com>
2016-05-12 22:10:43 -0400

    used to return what move a pokemon has

---
+++ Move.java
@@ -0,0 +1,114 @@
+
+public class Move {
+
+	private  String typename;
+	private String moveName;
+	
+	Move(){
+		this.typename = "null";
+		this.moveName  ="none";
+	}
+	Move(String name){
+		this.typename = name;
+	}
+	Move(String type,String move){
+		this.typename = type;
+		this.moveName = move;
+	}
+	public void setTypeName(String type){
+		this.typename = type;
+	}
+	public void setMovesList(String move){
+		this.moveName = move;
+	}
+	public String getTypeName(){
+		return this.typename;
+	}
+	public String getMove(){
+		return this.moveName;
+	}
+	public String toString(){
+		String toreturn = typename+"\t"+moveName;
+		return toreturn;
+	}
+	
+	
+	
+	public enum TYPES{
+		GRASS{
+			public String toString(){
+				return "Grass";
+			}
+		},
+		FIRE{
+			public String toString(){
+				return "Fire";
+			}
+		},
+		WATER{
+			public String toString(){
+				return "Water";
+			}
+		},
+		BUG{
+			public String toString(){
+				return "Bug";
+			}
+		},
+		NORMAL{
+			public String toString(){
+				return "Normal";
+			}
+		},
+		POISON{
+			public String toString(){
+				return "Poison";
+			}
+		},
+		ELECTRIC{
+			public String toString(){
+				return "Electric";
+			}
+		},
+		GROUND{
+			public String toString(){
+				return "Ground";
+			}
+		},
+		FAIRY{
+			public String toString(){
+				return "Fairy";
+			}
+		},
+		FIGHTING{
+			public String toString(){
+				return "Fighting";
+			}
+		},
+		ROCK{
+			public String toString(){
+				return "Rock";
+			}
+		},
+		PSYCHIC{
+			public String toString(){
+				return "Psychic";
+			}
+		},
+		GHOST{
+			public String toString(){
+				return "Ghost";
+			}
+		},
+		ICE{
+			public String toString(){
+				return "Ice";
+			}
+		},
+		DRAGON{
+			public String toString(){
+				return "Dragon";
+			}
+		}
+	}
+}
