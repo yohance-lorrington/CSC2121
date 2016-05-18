2b6bdae030e8669fa73ef7fdcf2480333a6c11a4
Adrian Logan <TheDude@CEN-0033417.local>
2016-05-18 15:11:23 -0400

    Moved main into driver class
    also make a few things for our attention

--- FillDataBase.java
+++ FillDataBase.java
@@ -20,129 +20,7 @@ public class FillDataBase {
 	public static final int FILLPOKEMONDATABASE =256;
 	public static final int FILLMOVEDATABASE = 128;
 
-	public static void main(String[] args) {
 	
-		final DataBaseHelper dbHelper = new DataBaseHelper();
-		try {
-			final Frame window = new Frame(dbHelper);
-			
-		// Invokes the GUI on a different thread. This way
-		// operations in each thread don't interfere with
-		// the other thread.
-		// Long database operations won't halt the GUI and
-		// any long operations on the GUI won't halt the main
-		// thread.
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					window.initialize();
-					window.PokeFrame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		
-		Scanner userInput = new Scanner(System.in);
-		int i = 0;
-
-		while(i!=-1){
-			System.out.println("\nWhat Would You like to do?");
-			System.out.println("Enter number corresponding to selection:\n "
-					+ "	1:Print All\n	2:Search by name\n	3:Search by ID\n"
-					+ "	4:Search by Type\n	5:Remove by ID\n	6: Fill Pokemon DB\n"
-					+ " 	7: Fill Moves DB\n 	8: Show Moves by Type\n 	9: Show Moves of Pokemon Name\n"
-					+ " \t10:Show All Moves\n"
-					+ "	11:Drop DB\n	-1:EXIT");
-
-			// In case an integer is not entered.
-			try {
-				i = userInput.nextInt();
-			} catch (java.util.InputMismatchException e) {
-				System.out.println("Please enter an integer!");
-
-				// Flush the last input from scanner.
-				// Prevents infinite loop.
-				userInput.next();
-				continue;
-			}
-			
-			switch (i) {
-				case 1: System.out.println("ALL POKEMONS");
-						for(Pokemon pokemon : dbHelper.gottaCatchEmAll()){
-							System.out.println(pokemon);
-						}
-						break;
-						
-				case 2: System.out.println("Please enter a Pokemon: ");
-						try {
-							System.out.println(dbHelper.getPokemon(userInput.next()));
-						} catch (java.lang.NullPointerException e) {
-							System.out.println("Pokemon does not exist!");
-						}
-						break;
-					
-				case 3: 
-						System.out.println("Please enter Pokemon ID: ");
-						try {
-							System.out.println(dbHelper.getPokemon(userInput.nextInt()));
-						} catch (java.util.InputMismatchException e) {
-
-						}
-						break;
-						
-				case 4: System.out.println("Please enter a Type: ");
-						for (Pokemon pokemon : dbHelper.getPokemonByType(userInput.next())) {
-							System.out.println(pokemon);
-						}
-						break;
-						
-				case 5: System.out.println("Please enter ID of Pokemon to remove: ");
-						dbHelper.deletePokemon(userInput.nextInt());
-						break;
-						
-				case 6: new FillDataBase(FillDataBase.FILLPOKEMONDATABASE);
-						break;
-
-				case 7: new FillDataBase(FillDataBase.FILLMOVEDATABASE);
-						break;
-
-				case 8: System.out.println("Please enter a Type: ");
-						for(Move move : dbHelper.getMoves(userInput.next())){
-							System.out.println(move);
-						}
-						break; 
-
-				case 9: System.out.println("Please enter a Pokemon Name: ");
-						for(Move move : dbHelper.getMoves(dbHelper.getPokemon(userInput.next()))){
-							System.out.println(move);
-						}
-						break;
-
-				case 10: System.out.println("ALL MOVES");
-						for(Move move : dbHelper.getAllMoves()){
-							System.out.println(move);
-						}
-						break;
-						
-				case 11: System.out.println("Dropping DaBase!");
-						dbHelper.removeDatabases();
-						break;
-
-			}
-		}
-
-		window.PokeFrame.dispatchEvent(
-				new WindowEvent(window.PokeFrame, WindowEvent.WINDOW_CLOSING)
-		);
-		userInput.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		dbHelper.closeConnection();
-	}
 	
 	//////////////////////////////////////////////////
 	// Utility Functions : {{{
@@ -232,7 +110,7 @@ public class FillDataBase {
 						db.insertPokemon(new Pokemon(Integer.valueOf(stuff[1]),stuff[2].toLowerCase(),stuff[3],stuff[4],stuff[5]));
 						
 					}
-					db.closeConnection();
+					//TODO db.closeConnection();
 				}
 				break;
 			case FILLMOVEDATABASE:
@@ -241,7 +119,7 @@ public class FillDataBase {
 					while((line = input.readLine()) != null){
 						line=line.replaceAll("\\s+", " ");
 						String[] stuff =line.split(" ");
-						for(int i =0;i<stuff.length;++i){
+						//TODO for(int i =0;i<stuff.length;++i){
 							if(stuff.length == 3){
 								db.insertTypeandMove(stuff[2], stuff[0]+" "+stuff[1]);
 								break;
@@ -250,10 +128,10 @@ public class FillDataBase {
 								db.insertTypeandMove(stuff[1], stuff[0]);
 								break;
 							}
-						}
+						//TODO }
 						
 					}
-					db.closeConnection();
+					//TODO db.closeConnection();
 				}
 				break;
 		}
